b4f54c89683651e455e270404055712ee2a1ce3b
Mohd Asri Omar <62752924+asriomar@users.noreply.github.com>
2024-05-06 15:23:54 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+
+    // JavaScript to toggle visibility of navigation menu on click
+    document.getElementById('hamburger').addEventListener('click', function () {
+        document.getElementById('navMenu').classList.toggle('hidden');
+    });
